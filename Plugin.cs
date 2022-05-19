ea4161231bb125973dab658d56d13524b5cabf38
MattDeDuck <Minihobbsch@gmail.com>
2022-05-19 18:21:02 +0100

    Initial upload

---
+++ Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace ToggleStir
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource Log { get; set; }
+        public static string pluginLoc = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static float speedSetting;
+        public static string toggleButton;
+        public JObject settingsObj;
+
+        public bool isActive = false;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            Log = this.Logger;
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        public void Update()
+        {
+            List<Potion.UsedComponent> cauldronIngredients = Managers.Potion.usedComponents;
+            if (cauldronIngredients.Count > 0)
+            {
+                // Get the toggle button
+                settingsObj = JObject.Parse(File.ReadAllText(pluginLoc + "/settings.json"));
+                toggleButton = (string)settingsObj["toggleButton"];
+
+                if (Input.GetKeyDown(toggleButton))
+                {
+                    isActive = !isActive;
+                }
+            }
+
+            if(isActive)
+            {
+                // Get the speed setting
+                settingsObj = JObject.Parse(File.ReadAllText(pluginLoc + "/settings.json"));
+                speedSetting = (float)settingsObj["speed"];
+
+                // Stir the cauldron
+                float num = speedSetting * Managers.RecipeMap.indicatorSettings.indicatorSpeed;
+                if (num > Mathf.Epsilon)
+                {
+                    Managers.RecipeMap.indicator.lengthToDeleteFromPath += num;
+                    if (Managers.Potion.potionStartedAt == null)
+                    {
+                        Managers.Potion.potionStartedAt = Managers.RecipeMap.currentMap;
+                    }
+                    if (Managers.Potion.usedComponents.Count > 0)
+                    {
+                        Books.GoalsBook.GoalsLoader.GetGoalByName("MoveIndicator", true).ProgressIncrement(1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Toggle Stir
+Toggle Stir is a mod made with BepInEx for use with the Potion Craft game. It gives you the ability to use a toggle key to stir your cauldron!
+
+### BepInEx
+- Download the latest BepInEx 5 release from [here](https://github.com/BepInEx/BepInEx/releases)
+Note: You need the `BepInEx_64` version for use with Potion Craft
+- You can read the installation for BepInEx [here](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+- Once installed run once to generate the config files and folders
+
+### Toggle Stir installation
+- Download the latest version from the [releases page](https://github.com/MattDeDuck/ToggleStir/releases)
+- Unzip the folder
+- Copy the folder into `Potion Craft/BepInEx/plugins/`
+- Run the game
+
+### Customise the settings
+The settings are found in `settings.json`
+- You can customise the speed of the stirring (float value) This is ranging from slowest `0.1` - fastest `1.0`
+- You can also customise the key you use for the toggle. You can see the naming conventions for the inputs [here](https://docs.unity3d.com/Manual/class-InputManager.html)
